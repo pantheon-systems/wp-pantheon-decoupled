6ea07ee7bd27e9e1588ebcceeaa9c5db83bb9dcc
CobyPear <coby.sher@pantheon.io>
2023-08-09 08:31:33 -0500

    DB-6464:
    - Moved set_default_options, show_example_preview_password_admin_notic, and delete_default_options
    to this plugin from the decoupled-preview plugin.
    - Added test and lint requirements to composer.json
    - Added test and lint scripts to composer.json
    - Port test from decoupled-preview to this plugin
    - Did not backfill tests for already existing functions

--- pantheon-decoupled-example.php
+++ pantheon-decoupled-example.php
@@ -15,6 +15,7 @@
 require_once(ABSPATH . 'wp-admin/includes/plugin.php');
 require_once(ABSPATH . 'wp-admin/includes/post.php');
 
+
 /**
  * Create a post when activating the plugin.
  */
@@ -65,20 +66,86 @@ function pantheon_decoupled_example_menu() {
 	$menu_locations = get_nav_menu_locations();
 	$menu_locations['footer'] = $menu_id;
 	set_theme_mod( 'nav_menu_locations', $menu_locations );
-	set_transient('pantheon_decoupled_example_menu_created', true);
+	set_transient( 'pantheon_decoupled_example_menu_created', true );
+}
+
+
+/**
+ * Show example preview password admin notice.
+ *
+ * @return void
+ */
+function show_example_preview_password_admin_notice() {
+	$preview_password = get_transient( 'example_preview_password' );
+	if ( $preview_password ) {
+		?>
+		<div class="notice notice-success notice-alt below-h2 is-dismissible">
+			<strong><?php esc_html_e( 'Pantheon Decoupled Preview Example', 'wp-decoupled-preview' ); ?></strong>
+			<p class="decoupled-preview-example">
+				<label for="new-decoupled-preview-example-value">
+					<?php echo wp_kses( __( 'The shared secret of the <strong>Example NextJS Preview</strong> site is:', 'wp-decoupled-preview' ), [ 'strong' => [] ] ); ?>
+				</label>
+				<input type="text" class="code" value="<?php printf( esc_attr( get_transient( 'example_preview_password' ) ) ); ?>" />
+			</p>
+			<p><?php esc_html_e( 'Be sure to save this in a safe location. You will not be able to retrieve it.', 'wp-decoupled-preview' ); ?></p>
+		</div>
+		<?php
+		delete_transient( 'example_preview_password' );
+	}
+}
+
+/**
+ * Delete preview sites options when deactivation plugin.
+ *
+ * @return void
+ */
+function delete_default_options() {
+	delete_option( 'preview_sites' );
+}
+
+/**
+ * Set default values for the preview sites options.
+ *
+ * @return void
+ */
+function set_default_options () {
+	$secret = wp_generate_password( 10, false );
+	set_transient( 'example_preview_password', $secret );
+
+	add_option(
+		'preview_sites',
+		[
+			'preview' => [
+				1 => [
+					'label' => esc_html__( 'Example NextJS Preview', 'wp-decoupled-preview' ),
+					'url' => 'https://example.com/api/preview',
+					'secret_string' => $secret,
+					'preview_type' => 'Next.js',
+					'associated_user' => 'decoupled_example_user',
+					'id' => 1,
+				],
+			],
+		]
+	);
 }
 
 /**
  * Activate the plugin.
  */
 function pantheon_decoupled_example_activate() {
-	if ( !post_exists( "Example Post with Image" ) ) {
-		if ( !get_transient( 'pantheon_decoupled_example_created' ) ) {
+	if ( ! post_exists( 'Example Post with Image' ) ) {
+		if ( ! get_transient( 'pantheon_decoupled_example_created' ) ) {
 			pantheon_decoupled_example_create_post();
 		}
-		if ( !get_transient( 'pantheon_decoupled_example_menu_created' ) ) {
+		if ( ! get_transient( 'pantheon_decoupled_example_menu_created' ) ) {
 			pantheon_decoupled_example_menu();
 		}
 	}
 }
+
+
+
 add_action( 'init', 'pantheon_decoupled_example_activate' );
+add_action( 'admin_notices', 'show_example_preview_password_admin_notice' );
+register_activation_hook( __FILE__, __NAMESPACE__ . '\\set_default_options' );
+register_deactivation_hook( __FILE__, __NAMESPACE__ . '\\delete_default_options' );
