aed5f962e3f124b9f8da5bec9ef92d9966818a16
Brian Perry <brian.perry@pantheon.io>
2023-09-25 17:15:24 -0500

    DB-6458: general FES settings cleanup

--- pantheon-decoupled.php
+++ pantheon-decoupled.php
@@ -610,7 +610,7 @@ function pantheon_decoupled_regen_env_vars() {
 	$preview_sites = get_option( 'preview_sites' );
 	$preview_site = isset( $preview_sites['preview'][ $id ] ) ? $preview_sites['preview'][ $id ] : null;
 
-	$docs_url = 'https://docs.pantheon.io/guides/decoupled/overview'
+	$docs_url = 'https://docs.pantheon.io/guides/decoupled/wp-nextjs-frontend-starters/content-preview'
 
 	?>
 	<style>
@@ -645,10 +645,9 @@ function pantheon_decoupled_regen_env_vars() {
 			);
 			?>
 		</a>
-		<p>Consult the
-			<a id="docs-link" target="_blank" href="<?php echo esc_url( $docs_url ); ?>">
-			Pantheon Documentation
-			</a> for more information about Front-End Site configuration.
+		<p>Consult the Pantheon Documentation for more information on
+			<a id="docs-link" target="_blank" rel='noopener noreferrer' href="<?php echo esc_url( $docs_url ); ?>">
+		configuring content preview</a>.
 		</p>
 			<?php
 		}
@@ -674,7 +673,7 @@ function pantheon_decoupled_regen_env_vars_action() {
 	$app_password = WP_Application_Passwords::create_new_application_password( get_user_by( 'login', $preview_site['associated_user'] )->ID, [ 'name' => 'preview-' . wp_generate_uuid4() ] );
 
 	$site_label = $preview_site['label'];
-	$docs_url = 'https://docs.pantheon.io/guides/decoupled/overview'
+	$docs_url = 'https://docs.pantheon.io/guides/decoupled/wp-nextjs-frontend-starters/content-preview'
 
 	?>
 	<style>
@@ -701,10 +700,10 @@ function pantheon_decoupled_regen_env_vars_action() {
 			</label>
 		</p>
 		<input type="text" class="code" size="30" value="<?php printf( esc_attr( WP_Application_Passwords::chunk_password( $app_password[0] ) ) ); ?>" />
-		<p>Consult the
-			<a id="docs-link" target="_blank" href="<?php echo esc_url( $docs_url ); ?>">
-			Pantheon Documentation
-			</a> for more information about Front-End Site configuration.
+		<p>Consult the Pantheon Documentation for more information on
+			<a id="docs-link" target="_blank" rel='noopener noreferrer' href="<?php echo esc_url( $docs_url ); ?>">
+		configuring content preview</a>.
+		</p>
 		</p>
 	</div>
 	<?php
--- src/class-fes-preview-table.php
+++ src/class-fes-preview-table.php
@@ -30,7 +30,7 @@ class FES_Preview_Table extends List_Table {
 				return isset( $item['content_type'] ) ? ucwords( implode( ', ', $item['content_type'] ) ) : __( 'Post, Page', 'wp-decoupled-preview' );
 			case 'actions':
 				return sprintf(
-					'<a href="%s&width=600&height=500" class="thickbox">%s</a> | <a href="%s&width=600&height=500" class="thickbox">%s</a> | <a href="%s&width=600&height=500" class="thickbox">%s</a> | <a href="%s&width=600&height=500" class="thickbox">%s</a> | <a href="%s&TB_iframe=true&width=600&height=500" class="thickbox">%s</a>',
+					'<a href="%s&width=600&height=500" class="thickbox">%s</a> | <a href="%s&width=600&height=500" class="thickbox">%s</a> | <a href="%s&width=600&height=500" class="thickbox">%s</a> | <a href="%s&TB_iframe=true&width=600&height=500" class="thickbox">%s</a> | <a href="%s&width=600&height=500" class="thickbox">%s</a>',
 					wp_nonce_url( add_query_arg( [
 						'page' => 'fes_add_preview_sites',
 						'action' => 'edit',
@@ -49,18 +49,18 @@ class FES_Preview_Table extends List_Table {
 						'id' => $item['id'],
 					], admin_url( 'options-general.php' ) ), 'env-vars', 'nonce' ),
 					esc_html__( 'Environment Variables', 'wp-decoupled-preview' ),
-					wp_nonce_url( add_query_arg( [
-						'page' => 'preview_delete',
-						'action' => 'delete',
-						'id' => $item['id'],
-					], admin_url( 'options-general.php' ) ), 'edit-preview-site', 'nonce' ),
-					esc_html__( 'Delete', 'wp-decoupled-preview' ),
 					wp_nonce_url( add_query_arg([
 						'page' => 'env_regen',
 						'action' => 'regen',
 						'id' => $item['id'],
 					], admin_url( 'options-general.php' ) ), 'env-regen', 'nonce' ),
-					esc_html__( 'Regenerate Variables', 'wp-decoupled-preview' )
+					esc_html__( 'Regenerate Variables', 'wp-decoupled-preview' ),
+					wp_nonce_url( add_query_arg( [
+						'page' => 'preview_delete',
+						'action' => 'delete',
+						'id' => $item['id'],
+					], admin_url( 'options-general.php' ) ), 'edit-preview-site', 'nonce' ),
+					esc_html__( 'Delete', 'wp-decoupled-preview' )
 				);
 			default:
 				return '';
